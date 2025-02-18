a9dfe4c4bf50cf11b914ecfde1e3fcb9a4f4c4ff
m4r1no0 <marinoosorio97@gmail.com>
2025-02-18 17:38:59 -0500

    inputs,tablas

---
+++ js/codigo.js
@@ -0,0 +1,42 @@
+const inputAgregar = document.querySelector('#nombreEquipo');
+const inputMarca = document.querySelector('#marca');
+const inputCant = document.querySelector('#cantidad');
+const inputNombre = document.querySelector('#nombreInstru');
+const inputId = document.querySelector('#idInstru');
+const cuerpo = document.querySelector('#cuerpoTabla');
+const save = document.querySelector('#guarda')
+const modal = document.querySelector('.modal-dialog');
+const black = document.querySelector('.modal-backdrop');
+let nombres = [];
+let marcas = [];
+let cantidades = [];
+
+    
+    
+    save.addEventListener('click',()=>{ 
+        let nombre = inputAgregar.value;
+        let marca = inputMarca.value;
+        let cant = inputCant.value;
+        nombres.push(nombre);
+        marcas.push(marca);
+        cantidades.push(cant);
+        let nuevoElemento = document.createElement('tr');
+        
+        const nuevoItem = document.createElement('td');
+            const marcaItem = document.createElement('td');
+            const cantItem = document.createElement('td');
+            cuerpo.appendChild(nuevoElemento);
+            nuevoElemento.appendChild(nuevoItem);
+            nuevoElemento.appendChild(marcaItem);
+            nuevoElemento.appendChild(cantItem);
+
+            nuevoItem.textContent = nombre;
+            marcaItem.textContent = marca;
+            cantItem.textContent = cant;
+
+        
+        
+
+        
+
+    })
